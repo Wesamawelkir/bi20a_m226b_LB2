0739a91f73d1ff4427623c73004c58d7bdbfe512
Wesamawelkir <wesam.awelkir@edu.tbz.ch>
2023-11-21 16:08:56 +0100

    noch nicht Finales Product wird noch Verbessert

--- SocialNetwork/src/NewsFeed.java
+++ SocialNetwork/src/NewsFeed.java
@@ -2,12 +2,8 @@ import java.util.ArrayList;
 
 public class NewsFeed {
 	
-	
-	private ArrayList<MessagePost> messages;
-	private ArrayList<PhotoPost> photos;
-	private ArrayList<EventPost> events;
-	
-	
+	public ArrayList<Post> meineListe = new ArrayList<Post>();
+
 	public NewsFeed() {
 		
 		messages = new ArrayList<>();
@@ -15,30 +11,16 @@ public class NewsFeed {
 	}
 	
 	
-	public void addMessagePost(MessagePost message) {
-		messages.add(message);
+	public void addPost(Post post) {
+		meineListe.add(post);
 	}
 	
-	public void addPhotoPost(PhotoPost photo) {
-		photos.add(photo);
-	}
-
-	public void addEventPost(EventPost events) {events.add(events)}
 	
 	public void show() {
-		
-		for(MessagePost message : messages) {
-			message.display();
-			System.out.println();
-		}
-		
-		for(PhotoPost photo : photos) {
-			photo.display();
-			System.out.println();
-		}
 
-		for(EventPost events : events) {
-			events.display();
+		Post[] Post;
+		for(Post post : Post) {
+			meineListe.display();
 			System.out.println();
 		}
 		
--- SocialNetwork/src/PhotoPost.java
+++ SocialNetwork/src/PhotoPost.java
@@ -8,15 +8,12 @@ import java.util.ArrayList;
  *
  */
 
-public class PhotoPost {
+public class PhotoPost extends Post {
 	
 	
-	private String username;
+
 	private String filename;
 	private String caption;
-	private long timestamp;
-	private int likes;
-	private ArrayList<String> comments;
 	
 	
 	/**
@@ -35,22 +32,9 @@ public class PhotoPost {
 		comments = new ArrayList<>();
 	}
 	
+
 	
-	public void like() {
-		likes++;
-	}
-	
-	
-	public void unlike() {
-		if (likes > 0){
-			likes--;
-		}
-	}
-	
-	public void addComment(String text) {
-		comments.add(text);
-	}
-	
+
 	public String getImageFile() {
 		return filename;
 	}
@@ -60,51 +44,11 @@ public class PhotoPost {
 		return caption;
 	}
 
-
-	public long getTimeStamp() {
-		return timestamp;
-	}
-	
 	/**
 	 * Simulate display for the time being
 	 */
-	public void display() {
-		
-		System.out.println("Username " + username);
-		System.out.println("FileName " + "{" + filename + "}");
-		System.out.println("Caption " + caption);
-		System.out.println("Posted " + timeString());
-		
-		if (likes > 0) {
-			System.out.println(likes + " people like this.");
-		} else {
-			System.out.println();
-		}
-		if (comments.isEmpty()) {
-			System.out.println("No Comments yet...");
-			
-		} else {
-			System.out.println("   " + comments.size() + " comment(s). Press C to view");
-		}
-	}
 
 
-
-	private String timeString() {
-		
-		long current = System.currentTimeMillis();
-		long pastMillis = current - timestamp;
-		long seconds = pastMillis/1000;
-		long minutes = seconds/60;
-		long hours = minutes/60;
-		if (minutes > 0) {
-			return minutes + " minutes ago";
-		} else if(hours > 0){
-			return hours + " hours ago";
-		} else {
-			return seconds + " seconds ago";
-		}
-		
-	}
 
 }
+
